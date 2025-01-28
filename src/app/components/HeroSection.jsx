05bda4e5a2bae48288c7889767b87937a1429a72
Amrutanshu <amrutanshumishra607@gmail.com>
2025-01-28 23:30:48 +0530

    second commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeroSection = () => {
+     return (
+          <section>
+               <div className='grid grid-cols-1 lg:grid-cols-12'>
+                    <div className='col-span-7'>
+                         <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I am Amrutanshu Mishra </h1>
+                         <p className='text-[#ADB7BE] text-lg lg:text-xl'>"Passionate about software development, I’m a B.Tech student at NIT Rourkela with a keen interest in
+                              competitive programming, full-stack development, and cutting-edge technologies like quantum
+                              computing. I thrive on creating innovative solutions and building impactful projects."
+                         </p>
+                    </div>
+                    
+               </div>
+               
+          </section>
+     )
+}
+
+export default HeroSection
